2ff202f75cd82f341719e326c2a538c1c0e5bb65
李宗政 <1964450730@qq.com>
2024-07-02 16:30:28 +0800

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
-        <Stack.Screen name="+not-found"/>
       </Stack>
     </ThemeProvider>
   );
